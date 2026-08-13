ba31cd248c8e6f17cf288a14b3882c04a5ca87d3
mgkwon <mg_kwon@naver.com>
2026-08-14 08:54:56 +0900

    feat(sri): add integrity generator and HTML tags (#217)

---
+++ src/tools/sri-generator/meta.js
@@ -0,0 +1,7 @@
+export default {
+  id: 'sri-generator',
+  name: 'SRI Generator',
+  description: 'Generate, validate, and format Subresource Integrity hashes and HTML tags.',
+  icon: '🔒',
+  category: 'Crypto',
+};
