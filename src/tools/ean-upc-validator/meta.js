d1b180c527b5bea93d9cadf3b35a803381412902
mgkwon <mg_kwon@naver.com>
2026-08-26 08:05:20 +0900

    feat(ean-upc-validator): add barcode validator (#299)

---
+++ src/tools/ean-upc-validator/meta.js
@@ -0,0 +1,7 @@
+export default {
+  id: 'ean-upc-validator',
+  name: 'EAN / UPC Validator',
+  description: 'Validate EAN-8, UPC-A, and EAN-13 barcode check digits in your browser.',
+  icon: '▥',
+  category: 'Tester',
+};
